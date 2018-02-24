5e37f451ad5192526f927aa815a654078042c10a
kakeru kaneko <csositai@gmail.com>
2018-02-24 14:28:28 +0000

    layout and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get 'home/top'
+  #root 'home#top'
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  #root 'application#hello'
 end
