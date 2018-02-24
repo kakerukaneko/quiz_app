89d13ec731f0a9638845a55df545e05d2c15004c
kakeru kaneko <csositai@gmail.com>
2018-02-24 04:06:19 +0000

    Initialize repositoryy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
+    render html: "You say 夏が胸を刺激する"
   end
 end
