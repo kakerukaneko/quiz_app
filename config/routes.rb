cb70f936574c4ee5b98004b752ed735c97a5cd3c
kakeru kaneko <csositai@gmail.com>
2018-02-25 12:44:53 +0000

    create signup,signin,myprofile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+  get "users/:id" => "users#show"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  get "login" => "users#login_form"
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  
+  
   get '/' => 'home#top'
   get 'about' => 'home#about'
 end
