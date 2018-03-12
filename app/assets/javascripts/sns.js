9d5338764cc3ac3108c01da26bbb86cafb0acb5e
kakeru kaneko <csositai@gmail.com>
2018-03-12 02:33:31 +0000

    [add] share button tumblr & LINE

--- app/assets/javascripts/sns.js
+++ app/assets/javascripts/sns.js
@@ -5,3 +5,6 @@
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+!function(d,s,id){var js,ajs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://assets.tumblr.com/share-button.js";ajs.parentNode.insertBefore(js,ajs);}}(document, "script", "tumblr-js");
