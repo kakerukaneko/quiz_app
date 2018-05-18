72c42d79bb3c3e710cd9e8e3df0dcae599cb2e3c
kakeru kaneko <csositai@gmail.com>
2018-05-18 06:11:36 +0000

    バリデーション群をまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   has_many :quiz
   has_many :like
-   
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 30 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
+  validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 },allow_nil: true
+  
+  before_save { self.email = email.downcase }
+  
+  has_secure_password
 end
