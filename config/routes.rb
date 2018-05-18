fa77c459082ddab4feb987e476f2ff9cd4463c94
kakeru kaneko <csositai@gmail.com>
2018-05-18 14:06:42 +0000

    onlyの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   root to: 'home#top'
   
-  resources :users do
+  resources :users, only: [:index, :create, :new, :edit, :show, :update] do
     collection do
       post 'login'
       post 'logout'
       get 'login_form'
-      post 'create'
-    end
-    member do
-      post :update
     end
   end
   
-  resource :contents, only: [:new,:edit,:create] do
+  resource :contents, only: [:new, :create] do
     get 'index'
     get 'answer'
     get 'result'
