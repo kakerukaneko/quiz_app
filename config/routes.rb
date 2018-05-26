18f69a450e09c05b490b159c120beef38f0c5f36
kakerukaneko <csositai@gmail.com>
2018-05-26 17:52:37 +0900

    新規登録　ログイン共にform_withに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: 'home#top'
-  
-  resources :users, only: %i[ index create new edit show update ] do
+
+  resources :users, only: %i[ index new create edit show update ] do
     collection do
       post 'login'
       post 'logout'
       get 'login_form'
     end
   end
-  
+
   resources :quizzes, only: %i[ index new create ] do
     collection do
       get 'answer'
